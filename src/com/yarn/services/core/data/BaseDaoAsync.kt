9e4ce1616c081e02106bf0306e4b0f0373b041d5
James Gliwa <jgliwa12@gmail.com>
2019-11-27 14:57:23 -0800

    test action commit

--- src/com/yarn/services/core/data/BaseDaoAsync.kt
+++ src/com/yarn/services/core/data/BaseDaoAsync.kt
@@ -7,27 +7,27 @@ import org.litote.kmongo.coroutine.CoroutineCollection
 import org.litote.kmongo.id.toId
 import org.litote.kmongo.util.idValue
 
-open class BaseDaoAsync<T: Any>(private val collection: CoroutineCollection<T>) :
+open class BaseDaoAsync<T : Any>(private val collection: CoroutineCollection<T>) :
 	IBaseDaoAsync<T> {
 
-    override suspend fun get(id: String): T? {
-        return get(ObjectId(id).toId())
-    }
+	override suspend fun get(id: String): T? {
+		return get(ObjectId(id).toId())
+	}
 
-    override suspend fun get(id: ObjectId): T? {
-        return get(id.toId())
-    }
+	override suspend fun get(id: ObjectId): T? {
+		return get(id.toId())
+	}
 
-    override suspend fun get(id: Id<T>): T? {
-        return collection.findOneById(id)
-    }
+	override suspend fun get(id: Id<T>): T? {
+		return collection.findOneById(id)
+	}
 
-    override suspend fun save(obj: T): Any? {
-        return collection.save(obj).idValue
-    }
+	override suspend fun save(obj: T): Any? {
+		return collection.save(obj).idValue
+	}
 
-    override suspend fun delete(id: Id<T>): Boolean {
-        return collection.deleteOneById(id).wasAcknowledged()
-    }
+	override suspend fun delete(id: Id<T>): Boolean {
+		return collection.deleteOneById(id).wasAcknowledged()
+	}
 
 }
